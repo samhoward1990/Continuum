4e3b01fe467a9a6036d6687ce964e229b46d5fdc
RobS <51037713+RobS30@users.noreply.github.com>
2019-08-01 21:03:22 -0500

    added nasa api call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // var queryURL = "https://images-api.nasa.gov/search?q="
-    // + searchLocation 
-
-    var queryURL ="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyBwnn85wMUw6B5v3IqxC2R54yYytQu2ChQ"
-
-    
-
-
-    // destination 
-    ;
-
-
+    $(document).on("click", ".destinationImage", function () {
+        $("#display-planets").empty();
+        var searchLocation = $(this).attr("id");
+        
+        var nasaURL = "https://images-api.nasa.gov/search?q="
+            + searchLocation;
+            console.log(searchLocation);
 
     $.ajax({
-        url: queryURL,
+        url: nasaURL,
         method: "GET"
     })
-        .then(function (response) {
-            console.log(queryURL);
-            console.log(response);
-            var results = response.data;
+        .then(function(nasaResponse){
+            console.log(nasaURL);
+            console.log(nasaResponse);
+            
+            for (var i=0; i < 4; i++) {
+                var nasaImageURL = nasaResponse.collection.items[i].links[0].href;
+                console.log(nasaImageURL);
+                var nasaImageDiv = $("<div>");
+                var nasaImage = $("<img>");
+                nasaImage.attr("src", nasaImageURL)
+                nasaImageDiv.append(nasaImage);
+                nasaImageDiv.appendTo($("#display-planets"));
+            }
+        });
+
+    });
+
 
 });
 
 
 
+//         .then(function (response) {
+//             console.log(queryURL);
+//             console.log(response);
+//             var results = response.data;
+//         });
 
-});
+//     var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyBwnn85wMUw6B5v3IqxC2R54yYytQu2ChQ"
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     })
+//         .then(function (response) {
+//             console.log(queryURL);
+//             console.log(response);
+//             var results = response.data;
+
+//         });
+
